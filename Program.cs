a0d6e4f060a0470565e54fe870453508336ca807
vpinob <46831704+vpinob@users.noreply.github.com>
2019-01-27 22:49:02 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,17 +8,20 @@ namespace App
 {
     class Program
     {
+        //Sets the console show state.
         [DllImport("user32.dll")]
         private static extern int ShowWindow(int Handle, int showState);
-
+        
+        //Get console handle
         [DllImport("kernel32.dll")]
         private static extern int GetConsoleWindow();
 
-        //invokes the method Tick() and continues to invoke it once per timer interval. 
+        //Invokes the method Tick() and continues to invoke it once per timer interval. 
         private static TimerCallback TimerCallback1;
        
         private static int win;
-
+        
+        //Timer1
         private static System.Threading.Timer Timer1;
 
         public static void Main()
@@ -34,13 +37,15 @@ namespace App
                 Timer1 = new System.Threading.Timer(TimerCallback1, null, 0, 100);
                 //Forever loop
                 for (; ; );
+                // Collect all generations of memory.
+                GC.Collect();
             }
             catch (Exception){ }
         }
 
         //Set the Cursor in a random position..
         //Get the screen resolution..
-        public static void Tick(Object stateInfo)
+        public static void Tick(Object obj)
         {
             Cursor.Position = new Point(new Random().Next() % Screen.PrimaryScreen.Bounds.Width,
                 new Random().Next() % Screen.PrimaryScreen.Bounds.Height);
